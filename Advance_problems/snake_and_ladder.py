d80016449b7e04025c921f263864c8a05a855431
varasheb <varkanthi456@gmail.com>
2024-06-13 10:58:19 +0530

    [Varshab] Added:UC-3 created options for game using random

--- Advance_problems/snake_and_ladder.py
+++ Advance_problems/snake_and_ladder.py
@@ -3,10 +3,22 @@ import random
 
 def roll_dice():
     return random.randint(1,6)
+
+def check_options():
+    return random.choice(['No play','Ladder','Snake'])
+
 def game():
     player_position=0
     dice_value=roll_dice()
-    player_position=dice_value
+    options=check_options()
+    if(options=='No play'):
+        pass
+    elif(options=='Ladder'):
+        player_position+=dice_value
+    else:
+        player_position-=dice_value
+    if(player_position<0):
+        player_position=0
     return player_position
 
 
