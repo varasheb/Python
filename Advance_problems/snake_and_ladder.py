5b70a455903bafd3c4c4fc814b597ee40ef97875
varasheb <varkanthi456@gmail.com>
2024-06-13 12:51:58 +0530

    [Varshab] Refactor:UC-7  game with 2 Player and Player get Ladder then  plays again

--- Advance_problems/snake_and_ladder.py
+++ Advance_problems/snake_and_ladder.py
@@ -18,45 +18,47 @@ def game():
 
     while True:
         for player in players:
-            dice_value = roll_dice()
-            options = check_options()
-            dice_rolls[player] += 1
+            while True:
+                dice_value = roll_dice()
+                options = check_options()
+                dice_rolls[player] += 1
 
-            if options == 'No play':
-                pass
-            elif options == 'Ladder':
-                if player_positions[player] + dice_value <= WINNING_POSITION:
-                    player_positions[player] += dice_value
-            else:
-                player_positions[player] -= dice_value
-                if player_positions[player] < 0:
-                    player_positions[player] = 0
+                if options == 'No play':
+                    break
+                elif options == 'Ladder':
+                    if player_positions[player] + dice_value <= WINNING_POSITION:
+                        player_positions[player] += dice_value
+                        if player_positions[player] == WINNING_POSITION:
+                            winning_player = player
+                            break
+                else:
+                    player_positions[player] -= dice_value
+                    if player_positions[player] < 0:
+                        player_positions[player] = 0
+                    break
 
-            if player == 1:
-                player1_positions.append((dice_value, player_positions[player]))
-            else:
-                player2_positions.append((dice_value, player_positions[player]))
+                if player == 1:
+                    player1_positions.append((dice_value, player_positions[player]))
+                else:
+                    player2_positions.append((dice_value, player_positions[player]))
 
-            if player_positions[player] == WINNING_POSITION:
-                winning_player = player
-                break
         if winning_player:
             break
 
     if winning_player == 1:
-        return 1,player1_positions ,player2_positions, dice_rolls
+        return 1, player1_positions, player2_positions, dice_rolls
     else:
-        return 2,player1_positions,player2_positions, dice_rolls
+        return 2, player1_positions, player2_positions, dice_rolls
 
 if __name__ == '__main__':
     player1 = input('Enter the name of player 1: ')
     player2 = input('Enter the name of player 2: ')
-    player, player1_positions,player2_positions, dice_rolls = game()
+    player, player1_positions, player2_positions, dice_rolls = game()
     print(f"Number of times dice was rolled for {player1} to win: {dice_rolls[1]}")
     print(f"Number of times dice was rolled for {player2} to win: {dice_rolls[2]}")
     print(f"Position after every die roll for {player1}: {player1_positions}")
     print(f"Position after every die roll for {player2}: {player2_positions}")
-    if(player==1):
+    if player == 1:
         print(f"{player1} won the game!")
     else:
         print(f"{player2} won the game!")
