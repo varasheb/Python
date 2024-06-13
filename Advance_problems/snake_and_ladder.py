ab3e816399321189ca81dc5dcc9e921cc6a3908b
varasheb <varkanthi456@gmail.com>
2024-06-13 11:28:24 +0530

    [Varshab] Added:UC-6 number of times the dice was played and positions

--- Advance_problems/snake_and_ladder.py
+++ Advance_problems/snake_and_ladder.py
@@ -10,9 +10,12 @@ def check_options():
 
 def game():
     player_position = 0
+    dice_rolls=0
+    positions=[(0,0)]
     while player_position < WINNING_POSITION:
         dice_value = roll_dice()
         options = check_options()
+        dice_rolls+=1
         if options == 'No play':
             pass
         elif options == 'Ladder':
@@ -22,7 +25,11 @@ def game():
             player_position -= dice_value
             if player_position < 0:
                 player_position = 0
-    return player_position
+        positions.append((dice_rolls,player_position))
+    return dice_rolls,positions
 
 if __name__ == '__main__':
-    print(game())
+    dice_rolls,positions=game()
+    print(f"Number of times dice was rolled to win: {dice_rolls}")
+    print(f"Position after every die roll:{positions}")
+
