a27edd75aae1473222536939c5760997c9bc145a
varasheb <varkanthi456@gmail.com>
2024-06-13 11:08:46 +0530

    [Varshab] Added:UC-4 Repeat till the Player reaches the winning position 100

--- Advance_problems/snake_and_ladder.py
+++ Advance_problems/snake_and_ladder.py
@@ -1,5 +1,6 @@
 import random
 
+WINNING_POSITION = 100
 
 def roll_dice():
     return random.randint(1,6)
@@ -8,19 +9,19 @@ def check_options():
     return random.choice(['No play','Ladder','Snake'])
 
 def game():
-    player_position=0
-    dice_value=roll_dice()
-    options=check_options()
-    if(options=='No play'):
-        pass
-    elif(options=='Ladder'):
-        player_position+=dice_value
-    else:
-        player_position-=dice_value
-    if(player_position<0):
-        player_position=0
+    player_position = 0
+    while player_position < WINNING_POSITION:
+        dice_value = roll_dice()
+        options = check_options()
+        if options == 'No play':
+            pass
+        elif options == 'Ladder':
+            player_position += dice_value
+        else:
+            player_position -= dice_value
+        if player_position < 0:
+            player_position = 0
     return player_position
-
 
 if __name__ == '__main__':
     print(game())
