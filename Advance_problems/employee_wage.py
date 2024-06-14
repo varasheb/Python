f56c96f7159a73eef77f0b262271ddb65297d812
varasheb <varkanthi456@gmail.com>
2024-06-14 17:25:39 +0530

    [Varshab] Added: UC-2 calculated  Daily Employee Wage

--- Advance_problems/employee_wage.py
+++ Advance_problems/employee_wage.py
@@ -1,14 +1,17 @@
 import random
 
 class Employee:
-    def __init__(self, name):
+    def __init__(self, name ,wage_per_hour =20,full_day_hours=8):
         self.name = name
+        self.wage_per_hour = wage_per_hour
+        self.full_day_hours = full_day_hours
 
     def check_attendance(self):
         attendance_status = random.randint(0, 1)
         return attendance_status
-
-
+    
+    def calculate_daily_wage(self):
+        return self.wage_per_hour * self.full_day_hours
 
 if __name__=='__main__':
     print("Welcome to Employee Wage Computation Program")
@@ -17,5 +20,7 @@ if __name__=='__main__':
     attendance = employee.check_attendance()
     if attendance == 1:
         print(f"{employee.name} is present.")
+        daily_wage = employee.calculate_daily_wage()
+        print(f"Daily wage of {employee.name}: {daily_wage}")
     else:
         print(f"{employee.name} is absent.")
