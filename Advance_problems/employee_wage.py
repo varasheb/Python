357356b66c1198ff9e53aa126f96ed40b1bc95f2
varasheb <varkanthi456@gmail.com>
2024-06-14 16:58:48 +0530

    [Varshab] Added: UC-1 Employee is Present or Absent

--- Advance_problems/employee_wage.py
+++ Advance_problems/employee_wage.py
@@ -1,2 +1,21 @@
+import random
+
+class Employee:
+    def __init__(self, name):
+        self.name = name
+
+    def check_attendance(self):
+        attendance_status = random.randint(0, 1)
+        return attendance_status
+
+
+
 if __name__=='__main__':
     print("Welcome to Employee Wage Computation Program")
+
+    employee = Employee("Varshab Kanthi")
+    attendance = employee.check_attendance()
+    if attendance == 1:
+        print(f"{employee.name} is present.")
+    else:
+        print(f"{employee.name} is absent.")
