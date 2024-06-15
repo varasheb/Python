db609171e7a96a53491cdbf14f9f7b076138b5d6
varasheb <varkanthi456@gmail.com>
2024-06-15 12:57:15 +0530

    [Varshab] Added:UC-2 check equality of two lines based on the end points

--- Advance_problems/line_comparision.py
+++ Advance_problems/line_comparision.py
@@ -9,6 +9,11 @@ class Line:
     
     def length(self):
         return math.sqrt((self.x2 - self.x1) ** 2 + (self.y2 - self.y1) ** 2)
+    def __eq__(self, other):
+        if not isinstance(other, Line):
+            return False
+        return self.x1 == other.x1 and self.y1 == other.y1 and self.x2 == other.x2 and self.y2 == other.y2
+
 
 
 
@@ -16,5 +21,16 @@ class Line:
 if __name__=="__main__":
     print("Welcome to Line Comparison Computation Program")
 
-    line = Line(1, 2, 4, 6)
-    print("Length of the line:", line.length())
+    line1 = Line(1, 2, 4, 6)
+    line2 = Line(1, 2, 4, 6)
+    line3 = Line(1, 2, 5, 5)
+    print("Length of the line1:", line1.length())
+    print("Length of the line2:", line2.length())
+    print("Length of the line3:", line3.length())
+
+
+    print("Line1 equals Line2:", line1 == line2) 
+    print("Line1 equals Line3:", line1 == line3)
+
+
+
