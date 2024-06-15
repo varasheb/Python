f3d496f35bff4af1ea80b3061f249abfa4d756f9
varasheb <varkanthi456@gmail.com>
2024-06-15 11:35:16 +0530

    [Varshab] Added: UC-3 Add Part time Employee & Wage

--- Advance_problems/employee_wage.py
+++ Advance_problems/employee_wage.py
@@ -1,26 +1,32 @@
 import random
 
 class Employee:
-    def __init__(self, name ,wage_per_hour =20,full_day_hours=8):
+    def __init__(self, name, wage_per_hour=200, full_day_hours=8, part_time_hours=4):
         self.name = name
         self.wage_per_hour = wage_per_hour
         self.full_day_hours = full_day_hours
+        self.part_time_hours = part_time_hours
 
     def check_attendance(self):
-        attendance_status = random.randint(0, 1)
+        attendance_status = random.randint(0, 2)
         return attendance_status
     
-    def calculate_daily_wage(self):
-        return self.wage_per_hour * self.full_day_hours
+    def calculate_wage(self, hours_worked):
+        return self.wage_per_hour * hours_worked
 
-if __name__=='__main__':
+if __name__ == '__main__':
     print("Welcome to Employee Wage Computation Program")
 
     employee = Employee("Varshab Kanthi")
     attendance = employee.check_attendance()
+
     if attendance == 1:
-        print(f"{employee.name} is present.")
-        daily_wage = employee.calculate_daily_wage()
-        print(f"Daily wage of {employee.name}: {daily_wage}")
+        print(f"{employee.name} is present full-time.")
+        daily_wage = employee.calculate_wage(employee.full_day_hours)
+        print(f"Daily wage of {employee.name}: Rs.{daily_wage}")
+    elif attendance == 2:
+        print(f"{employee.name} is present part-time.")
+        daily_wage = employee.calculate_wage(employee.part_time_hours)
+        print(f"Daily wage of {employee.name}: Rs.{daily_wage}")
     else:
         print(f"{employee.name} is absent.")
