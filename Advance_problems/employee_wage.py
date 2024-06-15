2bfa705d600a8f122d93824d79a566f6e788ca5e
varasheb <varkanthi456@gmail.com>
2024-06-15 12:15:35 +0530

    [Varshab] Added: UC-8 Compute Employee Wage for multiple companies

--- Advance_problems/employee_wage.py
+++ Advance_problems/employee_wage.py
@@ -1,7 +1,7 @@
 import random
 
 class Employee:
-    def __init__(self, name, wage_per_hour=200, full_day_hours=8, part_time_hours=4):
+    def __init__(self, name, wage_per_hour, full_day_hours, part_time_hours):
         self.name = name
         self.wage_per_hour = wage_per_hour
         self.full_day_hours = full_day_hours
@@ -33,12 +33,31 @@ class Employee:
                 total_days += 1  
 
         return total_wage,total_days,total_hours
-    
+class Company(Employee):
+    def __init__(self, company_name,name, wage_per_hour, full_day_hours=8, part_time_hours=4):
+        super().__init__(name, wage_per_hour, full_day_hours, part_time_hours)
+        self.company_name=company_name
+
+
 if __name__ == '__main__':
     print("Welcome to Employee Wage Computation Program")
 
-    employee = Employee("Varshab Kanthi")
-    monthly_report = employee.calculate_monthly_wage()
+    employees = [ 
+        Company("Google","Varshab Kanthi",1000),
+        Company("Intellicar", "Ramesh",400),
+        Company("Amazon", "Suresh",200),
+        Company("Microsoft", "Ganesh",230)
+                ]
+    for employee in employees:
+        monthly_report = employee.calculate_monthly_wage()
 
-    print(f"Monthly wage of {employee.name} for {monthly_report[1]} working days with {monthly_report[2]} hours worked: Rs.{monthly_report[0]:.2f}")
+        print(f"Monthly wage of {employee.company_name}'s employee {employee.name}:")
+        print(f"- For {monthly_report[1]} working days")
+        print(f"- With {monthly_report[2]} hours worked")
+        print(f"- Total wage: Rs.{monthly_report[0]:.2f}")
 
+
+
+
+
+                  
