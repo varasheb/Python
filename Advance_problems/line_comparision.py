87d6a9b7c484cbd875717790457b85fbb23b26b2
varasheb <varkanthi456@gmail.com>
2024-06-15 13:06:36 +0530

    [Varshab] Added:UC-3 compared two lines based on the end points for checking line is equal, greater or less than the other line

--- Advance_problems/line_comparision.py
+++ Advance_problems/line_comparision.py
@@ -8,11 +8,16 @@ class Line:
         self.y2 = y2
     
     def length(self):
-        return math.sqrt((self.x2 - self.x1) ** 2 + (self.y2 - self.y1) ** 2)
+        return round(math.sqrt((self.x2 - self.x1) ** 2 + (self.y2 - self.y1) ** 2), 4)
     def __eq__(self, other):
         if not isinstance(other, Line):
             return False
         return self.x1 == other.x1 and self.y1 == other.y1 and self.x2 == other.x2 and self.y2 == other.y2
+    def __lt__(self, other):
+        return self.length() < other.length()
+
+    def __gt__(self, other):
+        return self.length() > other.length()
 
 
 
@@ -23,14 +28,16 @@ if __name__=="__main__":
 
     line1 = Line(1, 2, 4, 6)
     line2 = Line(1, 2, 4, 6)
-    line3 = Line(1, 2, 5, 5)
+    line3 = Line(1, 2, 5, 6)
     print("Length of the line1:", line1.length())
     print("Length of the line2:", line2.length())
     print("Length of the line3:", line3.length())
 
-
+    print("Checking equality of two lines")
     print("Line1 equals Line2:", line1 == line2) 
     print("Line1 equals Line3:", line1 == line3)
 
-
+    print("Comparing two lines")
+    print("Line1 is greater than Line3:", line1 > line3)
+    print("Line1 is less than Line3:", line1 < line3)
 
