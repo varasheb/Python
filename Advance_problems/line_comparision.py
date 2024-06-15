4b88a0eef32d7b5ad7aba189b2346a567f34e560
varasheb <varkanthi456@gmail.com>
2024-06-15 12:45:14 +0530

    [Varshab] Added:UC-1 model a line based on points (x,y) and finded its length

--- Advance_problems/line_comparision.py
+++ Advance_problems/line_comparision.py
@@ -1,2 +1,20 @@
-print("Welcome to Line Comparison Computation Program")
+import math
 
+class Line:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+    
+    def length(self):
+        return math.sqrt((self.x2 - self.x1) ** 2 + (self.y2 - self.y1) ** 2)
+
+
+
+
+if __name__=="__main__":
+    print("Welcome to Line Comparison Computation Program")
+
+    line = Line(1, 2, 4, 6)
+    print("Length of the line:", line.length())
